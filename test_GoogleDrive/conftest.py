2a237db88e03cd7c426b190a0d79deffd13b31e5
anand <anand3007@hotmail.com>
2020-09-30 10:44:25 +0530

    latest

--- test_GoogleDrive/conftest.py
+++ test_GoogleDrive/conftest.py
@@ -1,54 +1,53 @@
 import pytest
 from selenium import webdriver
 
+
 def pytest_addoption(parser):
     parser.addoption(
         "--browser_name", action="store", default="chrome"
     )
+
 
 @pytest.fixture(scope="function")
 def setup(request):
     global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
-        driver = webdriver.Chrome(
-            executable_path="/Users/nagarajualapati/PycharmProjects/Google_Pytest_e2e/driver/chromedriver")
+        driver = webdriver.Chrome()
     elif browser_name == "firefox":
-        driver = webdriver.Firefox(
-            executable_path="/Users/nagarajualapati/PycharmProjects/Google_Pytest_e2e/driver/geckodriver")
+        driver = webdriver.Firefox()
     elif browser_name == "edge":
         driver = webdriver.Ie(
             executable_path="/Users/nagarajualapati/PycharmProjects/Google_Pytest_e2e/driver/msedgedriver")
-    driver.get("https://www.google.com/intl/en_in/drive/")
+    # driver.get("https://www.google.com/intl/en_in/drive/")
     driver.maximize_window()
     request.cls.driver = driver
     yield
     driver.quit()
 
 
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
 
-# @pytest.mark.hookwrapper
-# def pytest_runtest_makereport(item):
-#     """
-#         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-#         :param item:
-#         """
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
-#
-#     if report.when == 'call' or report.when == "setup":
-#         xfail = hasattr(report, 'wasxfail')
-#         if (report.skipped and xfail) or (report.failed and not xfail):
-#             file_name = report.nodeid.replace("::", "_") + ".png"
-#             _capture_screenshot(file_name)
-#             if file_name:
-#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
-#                 extra.append(pytest_html.extras.html(html))
-#         report.extra = extra
-#
-#
-# def _capture_screenshot(name):
-#     driver.get_screenshot_as_file(name)
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
--- test_GoogleDrive/test_driveLogin.py
+++ test_GoogleDrive/test_driveLogin.py
@@ -10,19 +10,6 @@ from PyTest.PageObject.DriveHomePage import GoogleDrivePage
 from PyTest.PageObject.DriveLogin import LoginPage
 from PyTest.PageObject.GoogleDoc import DocHomePage
 
-
-# @pytest.fixture()
-# def test_setup():
-#     global driver
-#     driver = webdriver.Chrome()
-#     driver.maximize_window()
-#     driver.implicitly_wait(5)
-#     yield
-#     driver.close()
-#     driver.switch_to.window(driver.window_handles[1])
-#     driver.close()
-#     driver.switch_to.window(driver.window_handles[0])
-#     driver.close()
 from PyTest.utilities.BaseClass import BaseClass
 
 
@@ -30,6 +17,7 @@ class TestGoogleDrive(BaseClass):
 
     def test_Login_To_Google_Sheet(self):
         self.driver.get("https://drive.google.com")
+        self.driver.implicitly_wait(10)
         print(self.driver.title)
         self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
         self.driver.switch_to.window(self.driver.window_handles[1])
@@ -57,13 +45,13 @@ class TestGoogleDrive(BaseClass):
             "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/Sheettitleupdated.png")
         time.sleep(3)
 
-
     def test_Login_to_Google_Doc(self):
         self.driver.get("https://drive.google.com")
         print(self.driver.title)
         self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
         self.driver.switch_to.window(self.driver.window_handles[1])
         login = LoginPage(self.driver)
+        time.sleep(2)
         login.enter_username("testuser30071")
         login.click_on_next_button()
         login.enter_password("Test@3007")
@@ -88,60 +76,50 @@ class TestGoogleDrive(BaseClass):
         self.driver.get_screenshot_as_file(
             "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/DocTitleUpdated.png")
 
+    def test_Login_to_Google_slides(self):
+        self.driver.get("https://drive.google.com")
+        print(self.driver.title)
+        self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        login = LoginPage(self.driver)
+        login.enter_username("testuser30071")
+        login.click_on_next_button()
+        login.enter_password("Test@3007")
+        login.click_on_pwd_next_button()
+        time.sleep(2)
+        drive = GoogleDrivePage(self.driver)
+        time.sleep(2)
+        drive.click_new_button()
+        drive.click_google_slide()
+        print("welcome to Google Slide")
+        self.driver.switch_to.window(self.driver.window_handles[2])
+        print(self.driver.title)
+        self.driver.find_element_by_css_selector("input[class='docs-title-input']").click()
+        self.driver.implicitly_wait(3)
+        act = ActionChains(self.driver)
+        act.send_keys(Keys.BACKSPACE).perform()
+        self.driver.get_screenshot_as_file(
+            "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/SlideTiltleCleared.png")
+        self.driver.find_element_by_xpath("//*[@id='docs-title-widget']/input").send_keys("Automation Slide Experiment")
+        self.driver.get_screenshot_as_file(
+            "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/SlideTitleUpdated.png")
 
-    # def test_Login_to_Google_slides(self):
-    #     self.driver.get("https://drive.google.com")
-    #     print(self.driver.title)
-    #     self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
-    #     #self.driver.switch_to.window(self.driver.window_handles[1])
-    #     login = LoginPage(self.driver)
-    #     login.enter_username("testuser30071")
-    #     login.click_on_next_button()
-    #     login.enter_password("Test@3007")
-    #     login.click_on_pwd_next_button()
-    #     time.sleep(2)
-    #     drive = GoogleDrivePage(self.driver)
-    #     time.sleep(2)
-    #     drive.click_new_button()
-    #     drive.click_google_slide()
-    #     print("welcome to Google Slide")
-    #     self.driver.switch_to.window(self.driver.window_handles[2])
-    #     print(self.driver.title)
-    #     self.driver.find_element_by_css_selector("input[class='docs-title-input']").click()
-    #     self.driver.implicitly_wait(3)
-    #     act = ActionChains(self.driver)
-    #     act.send_keys(Keys.BACKSPACE).perform()
-    #     self.driver.get_screenshot_as_file(
-    #         "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/SlideTiltleCleared.png")
-    #     self.driver.find_element_by_xpath("//*[@id='docs-title-widget']/input").send_keys("Automation Slide Experiment")
-    #     self.driver.get_screenshot_as_file(
-    #         "/Users/priyamvadaanand/PycharmProjects/google_selenium/POM/screenshots/SlideTitleUpdated.png")
-    #
-    #
-    #
-    # def test_Login_to_Google_createfolder(self):
-    #     self.driver.get("https://drive.google.com")
-    #     print(self.driver.title)
-    #     self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
-    #     #self.driver.switch_to.window(self.driver.window_handles[1])
-    #     login = LoginPage(self.driver)
-    #     login.enter_username("testuser30071")
-    #     login.click_on_next_button()
-    #     login.enter_password("Test@3007")
-    #     login.click_on_pwd_next_button()
-    #     drive = GoogleDrivePage(self.driver)
-    #     time.sleep(2)
-    #     drive.click_new_button()
-    #     drive.create_folder("testcase6")
-    #     time.sleep(2)
-    #     drive.click_new_button()
-    #     drive.create_folder("testcase7")
-    #
-    #     time.sleep(5)
+    def test_Login_to_Google_createfolder(self):
+        self.driver.get("https://drive.google.com")
+        print(self.driver.title)
+        self.driver.find_element_by_xpath("//header//div[3]//div[1]//a[1]").click()
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        login = LoginPage(self.driver)
+        login.enter_username("testuser30071")
+        login.click_on_next_button()
+        login.enter_password("Test@3007")
+        login.click_on_pwd_next_button()
+        drive = GoogleDrivePage(self.driver)
+        time.sleep(2)
+        drive.click_new_button()
+        drive.create_folder("testcase6")
+        time.sleep(2)
+        drive.click_new_button()
+        drive.create_folder("testcase7")
 
-
-    # @classmethod
-    # # def tearDownClass(cls):
-    # def tearDown(self):
-    #     self.driver.quit()
-
+        time.sleep(5)
